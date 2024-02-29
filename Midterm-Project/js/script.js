69fc4a5a3ffc4fe07482a3e82928b06a9d091484
katieclode <86265300+katieclode@users.noreply.github.com>
2024-02-28 20:51:50 -0700

    Add files via upload

---
+++ Midterm-Project/js/script.js
@@ -0,0 +1,32 @@
+
+// // call the init function when the page is done loading
+// $(init);
+
+// function init() {
+//     // calls function to find season when button is clicked
+//     $("#thumbnail").click(); 
+// }
+
+const thumbnail = document.getElementById('thumbnail');
+const lightbox = document.getElementById('lightbox');
+const closeBtn = document.getElementById('close-btn');
+const videoEmbed = document.getElementById('video-embed');
+
+thumbnail.addEventListener('click', function() {
+    lightbox.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', function() {
+    closeLightbox();
+});
+
+lightbox.addEventListener('click', function(e) {
+    if (e.target === lightbox) {
+        closeLightbox();
+    }
+});
+
+function closeLightbox() {
+    lightbox.classList.add('hidden');
+    videoEmbed.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+}
